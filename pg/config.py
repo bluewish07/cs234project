28feb44f2fc348366acc4e0ab71b61ac1ade810a
Xinyuan Huang <hxydiana@gmail.com>
2018-02-27 23:54:37 -0800

    added config for render so that you can turn render off for azure

--- pg/config.py
+++ pg/config.py
@@ -5,6 +5,7 @@ class config():
     env_name = "simple_spread"
     algo_name = "PG"
     record           = False
+    render = False
 
     # output config
     output_path  = "../results/" + env_name + "/" + algo_name + "/"
