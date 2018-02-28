f74fd52a303f5ae90f6f83f147e6f57f60476350
Xinyuan Huang <hxydiana@gmail.com>
2018-02-27 23:02:21 -0800

    added batch_size_in_episodes and tweaked pg config

--- pg/config.py
+++ pg/config.py
@@ -18,16 +18,17 @@ class config():
     
     # model and training config
     num_batches = 200 # number of batches trained on 
-    batch_size = 1000 # number of steps used to compute each policy update
+    batch_size = 1024 # number of steps used to compute each policy update
+    batch_size_in_episodes = 50 # number of episodes used to compute each policy update
     # TODO - update configs to represent # of episodes per policy update
-    max_ep_len = -1 # maximum episode length
+    max_ep_len = 1000 # maximum episode length
     learning_rate = 1e-3
     gamma              = 1.0 # the discount factor
     use_baseline = True 
     normalize_advantage=True 
     # parameters for the policy and baseline models
-    n_layers = 1
-    layer_size = 16 
+    n_layers = 2
+    layer_size = 128
     activation=tf.nn.relu 
 
 
